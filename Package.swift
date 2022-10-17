ee51ac9b645284239714148d0752b23eabda7bb6
YYudai <40518082+YYudai@users.noreply.github.com>
2022-10-18 00:06:20 +0900

    Feature: 改行を追加

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(
             url: "https://github.com/YYudai/SwiftConvenienceKit",
-            branch: "main"),
+            branch: "main"
+        ),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
